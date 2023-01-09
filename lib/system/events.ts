ccc5c15d4b6957092bfd6497e00a66e868f2a963
PHO <pho@cielonegro.org>
2023-01-09 22:45:26 +0900

    lib/watchdog.ts: Re-added

--- lib/system/events.ts
+++ lib/system/events.ts
@@ -1,12 +1,15 @@
-import { IEventSignal, PassThruEventSignal } from "../event.js";
+import { IEventSignal, GluedEventSignal } from "../event.js";
+import { BeforeWatchdogTerminateEvent } from "../watchdog.js";
 import * as MC from "@minecraft/server";
 
 export class SystemEvents {
-    public readonly beforeWatchdogTerminate: IEventSignal<MC.BeforeWatchdogTerminateEvent>;
+    public readonly beforeWatchdogTerminate: IEventSignal<BeforeWatchdogTerminateEvent>;
 
     public constructor(rawSystem: MC.System) {
         const rawEvents = rawSystem.events;
 
-        this.beforeWatchdogTerminate = new PassThruEventSignal(rawEvents.beforeWatchdogTerminate);
+        this.beforeWatchdogTerminate = new GluedEventSignal(rawEvents.beforeWatchdogTerminate, (rawEv: MC.BeforeWatchdogTerminateEvent) => {
+            return new BeforeWatchdogTerminateEvent(rawEv);
+        });
     }
 }
