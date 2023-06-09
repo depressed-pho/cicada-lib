cdef33c323cf5dcb746d0d236a3738b26f541fc9
PHO <pho@cielonegro.org>
2023-06-09 12:06:04 +0900

    Fix an incompatibility with 1.20.0 API

--- lib/system.ts
+++ lib/system.ts
@@ -12,8 +12,21 @@ export class System extends Wrapper<MC.System> {
     /** Package private: user code should not use this. */
     public constructor(rawSystem: MC.System) {
         super(rawSystem);
+
+        // FIXME: In MCBE 1.20.0 MC.System has not transitioned to
+        // after/before events yet. Delete this glue code when it's
+        // updated.
+        this.afterEvents  = new SystemAfterEvents({
+            scriptEventReceive: (this.raw as any).events.scriptEventReceive
+        } as any);
+        this.beforeEvents = new SystemBeforeEvents({
+            watchdogTerminate: (this.raw as any).events.beforeWatchdogTerminate
+        } as any);
+
+        /* New API
         this.afterEvents  = new SystemAfterEvents(this.raw.afterEvents);
         this.beforeEvents = new SystemBeforeEvents(this.raw.beforeEvents);
+        */
     }
 }
 
