5908096bfa7875b113a1ce3fd4f80eb37dfc186d
PHO <pho@cielonegro.org>
2023-11-13 20:10:07 +0900

    lib/sync/mpsc.ts: New method: receiveAll

--- lib/sync/mpsc.ts
+++ lib/sync/mpsc.ts
@@ -1,50 +1,85 @@
 import { Queue } from "../queue.js";
 import { Notify } from "./notify.js";
 
+export class ChannelBase<T> {
+    protected queue: Queue<T>;
+    protected readonly sent: Notify;
+
+    public constructor() {
+        this.queue = Queue.empty;
+        this.sent  = new Notify();
+    }
+
+    public async receive(): Promise<T> {
+        while (true) {
+            if (this.queue.isEmpty) {
+                await this.sent.notified();
+                continue;
+            }
+            else {
+                const [head, rest] = this.queue.uncons();
+                this.queue = rest;
+                return head;
+            }
+        }
+    }
+
+    /** If the queue is empty, block until at least one message is
+     * sent. Otherwise return all the messages in the queue.
+     */
+    public async receiveAll(): Promise<Queue<T>> {
+        while (true) {
+            if (this.queue.isEmpty) {
+                await this.sent.notified();
+                continue;
+            }
+            else {
+                const messages = this.queue;
+                this.queue = Queue.empty;
+                return messages;
+            }
+        }
+    }
+}
+
 /** A bounded multi-producer, single-consumer queue for sending values
  * between threads. The bounded variant has a limit on the number of
  * messages that the channel can store, and if this limit is reached,
  * trying to send another message will wait until a message is received
  * from the channel.
  */
-export class Channel<T> {
+export class Channel<T> extends ChannelBase<T> {
     readonly limit: number;
-    #queue: Queue<T>;
-    readonly #sent: Notify;
     readonly #received: Notify;
 
     public constructor(limit: number) {
+        super();
         this.limit     = limit;
-        this.#queue    = Queue.empty;
-        this.#sent     = new Notify();
         this.#received = new Notify();
+    }
+
+    public override async receive(): Promise<T> {
+        const message = await super.receive();
+        this.#received.notifyAll();
+        return message;
+    }
+
+    public override async receiveAll(): Promise<Queue<T>> {
+        const messages = await super.receiveAll();
+        this.#received.notifyAll();
+        return messages;
     }
 
     public async send(value: T): Promise<void> {
         while (true) {
-            if (this.#queue.length < this.limit) {
-                this.#queue = this.#queue.snoc(value);
-                this.#sent.notifyAll();
+            if (this.queue.length < this.limit) {
+                this.queue = this.queue.snoc(value);
+                this.sent.notifyAll();
                 return;
             }
             else {
                 await this.#received.notified();
                 continue;
-            }
-        }
-    }
-
-    public async receive(): Promise<T> {
-        while (true) {
-            if (this.#queue.isEmpty) {
-                await this.#sent.notified();
-                continue;
-            }
-            else {
-                const [head, rest] = this.#queue.uncons();
-                this.#queue = rest;
-                this.#received.notifyOne();
-                return head;
             }
         }
     }
@@ -55,34 +90,9 @@ export class Channel<T> {
  * send method will always complete immediately. This makes the
  * `UnboundedChannel` usable from both synchronous and asynchronous code.
  */
-export class UnboundedChannel<T> {
-    #queue: Queue<T>;
-    readonly #sent: Notify;
-    readonly #received: Notify;
-
-    public constructor() {
-        this.#queue    = Queue.empty;
-        this.#sent     = new Notify();
-        this.#received = new Notify();
-    }
-
+export class UnboundedChannel<T> extends ChannelBase<T> {
     public send(value: T) {
-        this.#queue = this.#queue.snoc(value);
-        this.#sent.notifyAll();
-    }
-
-    public async receive(): Promise<T> {
-        while (true) {
-            if (this.#queue.isEmpty) {
-                await this.#sent.notified();
-                continue;
-            }
-            else {
-                const [head, rest] = this.#queue.uncons();
-                this.#queue = rest;
-                this.#received.notifyOne();
-                return head;
-            }
-        }
+        this.queue = this.queue.snoc(value);
+        this.sent.notifyAll();
     }
 }
